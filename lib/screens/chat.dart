165c311447d46bc4dcffe7dfc88fa10bf2d7fbd7
timaevV <timaevvladislav@gmail.com>
2023-08-17 03:06:35 +0600

    add:store

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat/constants.dart';
+import 'package:flutter_chat/store/constants.dart';
 
 class ChatScreen extends StatefulWidget {
   static String route = "chat";
