025b80b54fa07cffd7f5a0a52c4bc1f98bc0de9f
timaevV <timaevvladislav@gmail.com>
2023-08-26 01:41:21 +0600

    add:config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:flutter_chat/screens/registration.dart';
 import 'package:flutter_chat/screens/welcome.dart';
 import 'package:flutter_chat/screens/login.dart';
 import 'package:flutter_chat/screens/chat.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyAQBw57Vy54frVZ1eUS2vxNQsecDoqWH4I", appId: "1:755012531996:android:ffdc29be0ad02655c0acd9", messagingSenderId: "755012531996", projectId: "flutter-chat-b2331"));
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
